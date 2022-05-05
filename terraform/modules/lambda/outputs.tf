55a11c35d06c885e5167b8b67fdc0758637587e6
windWingWalker <elliot562k@gmail.com>
2022-05-05 19:38:17 +0800

    refactor terraform

--- terraform/modules/lambda/outputs.tf
+++ terraform/modules/lambda/outputs.tf
@@ -1,16 +1,4 @@
-output "function_name" {  
-  description = "Name of the Lambda function."
-  value = aws_lambda_function.default.function_name
+output "function_arn" {  
+    description = "ARN of the Lambda function."
+    value = aws_lambda_function.default.arn
 }
-
-output "function_invoke_arn" {  
-  description = "Arn of the Lambda function."
-  value = aws_lambda_function.default.invoke_arn
-}
-
-output "ecr_uri" {
-  value = aws_ecr_repository.default.repository_url
-}
-
-
-
